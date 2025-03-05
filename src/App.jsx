aec49a72818ce71a20a8dda65f14f453c27e7363
nguyenphuoc1509 <nguyenphuoc150903@gmail.com>
2025-03-05 21:20:33 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/router";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 function App() {
   return (
-    <Provider store={store}>
+    <>
       <RouterProvider router={router} />
-    </Provider>
+    </>
   );
 }
 
